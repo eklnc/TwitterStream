c28fc952b4ea84d2786f971b34efc8251c9dc6f4
Emir Kılınç <emirkilinc@hotmail.com>
2019-03-28 11:52:21 +0300

    Edit program cs

--- TwitterStream/Program.cs
+++ TwitterStream/Program.cs
@@ -8,21 +8,24 @@ namespace TwitterStream
     {
         static void Main(string[] args)
         {
-            //using (var context = new TwitterStreamContext())
-            //{
-            //    TwitterStream stream = new TwitterStream(context);
+            using (var context = new TwitterStreamContext())
+            {
+                TwitterStream stream = new TwitterStream(context);
 
-            //    try
-            //    {
-            //        stream.Start();
-            //    }
-            //    catch (WebException ex)
-            //    {
-            //        Console.WriteLine("***HATA ALINDI ***: " + ex.Message + "\n\n");
+                try
+                {
+                    //stream.Start();
 
-            //        stream.InsertError(ex);
-            //    }
-            //}
+                    // gereksiz kodlar
+                    var asasdasd = stream.ToString();
+                }
+                catch (WebException ex)
+                {
+                    Console.WriteLine("***HATA ALINDI ***: " + ex.Message + "\n\n");
+
+                    stream.InsertError(ex);
+                }
+            }
         }
     }
 }
